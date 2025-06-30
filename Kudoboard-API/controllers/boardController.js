1d22c2879101b69ae39a59f3f2d920e7704771f5
jandresol <jasmine.andresol@gmail.com>
2025-06-30 14:19:13 -0700

    boardcontroller setup

--- Kudoboard-API/controllers/boardController.js
+++ Kudoboard-API/controllers/boardController.js
@@ -13,52 +13,52 @@ exports.getAll = async (req, res) => {
     }
 
     try {
-        const products = await prisma.product.findMany({
+        const boards = await prisma.board.findMany({
             where: filters,
         });
 
-        res.json(products);
+        res.json(boards);
     } catch (err) {
         console.error(err);
-        res.status(500).json({ error: "Failed to fetch products" });
+        res.status(500).json({ error: "Failed to fetch boards" });
     }
 };
 
 exports.getById = async (req, res) => {
     const id = Number(req.params.id)
-    const product = await prisma.product.findUnique({where : { id }});
-    if (!product) return res.status(404).json({ error: "Not found!" });
-    res.json(product);
+    const board = await prisma.board.findUnique({where : { id }});
+    if (!board) return res.status(404).json({ error: "Not found!" });
+    res.json(board);
 }
 
-// Post /products
+// Post /boards
 exports.create = async (req, res) => {
     const {name, description, price, image_url, category } = req.body
     if (!name || !description || !price || !category) {
         return res.status(400).json({ error: "Missing required fields" });
     }
 
-    const newProduct = await prisma.product.create({
+    const newboard = await prisma.board.create({
         data: {name, description, price, image_url, category}
     });
-    res.status(201).json(newProduct)
+    res.status(201).json(newboard)
 }
 
-// Put /products/:id
+// Put /boards/:id
 exports.update = async (req, res) => {
     const id = Number(req.params.id)
     const { name, description, price, image_url, category } = req.body
-    const updatedProduct = await prisma.product.update({
+    const updatedboard = await prisma.board.update({
         where: { id },
         data: { name, description, price, image_url, category },
     })
-    res.json(updatedProduct)
+    res.json(updatedboard)
 }
 
 // Delete
 exports.remove = async (req, res) => {
     const id = Number(req.params.id);
-    await prisma.product.delete ({ where : { id }})
+    await prisma.board.delete ({ where : { id }})
     res.status(204).end();
 }
 
