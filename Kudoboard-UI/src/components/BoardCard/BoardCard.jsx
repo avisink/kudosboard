d1578f56020a9a42d383c8255bbfba9f3acdcb7c
jandresol <jasmine.andresol@gmail.com>
2025-07-01 09:53:19 -0700

    boards show up on page

--- Kudoboard-UI/src/components/BoardCard/BoardCard.jsx
+++ Kudoboard-UI/src/components/BoardCard/BoardCard.jsx
@@ -2,7 +2,7 @@
 import "./BoardCard.css"
 import { useNavigate } from "react-router-dom";
 
-function BoardCard () {
+function BoardCard ({ board }) {
     const navigate = useNavigate();
     return (
         <>
@@ -13,6 +13,11 @@ function BoardCard () {
                 </img>
             </div>
             <div className="info">
+                <h3>{board.title}</h3>
+                <p>{board.description}</p>
+                <span className="category">{board.category}</span>
+            </div>
+            <div className="buttons">
                 <button className="option" onClick={() => navigate("/kudos")}>view</button>
                 <button className="option">delete</button>
             </div> 
