141e13f5dcfd453a0c008a886b5e8d75b4036053
jandresol <jasmine.andresol@gmail.com>
2025-07-02 11:04:42 -0700

    ui changes

--- Kudoboard-UI/src/components/BoardCard/BoardCard.jsx
+++ Kudoboard-UI/src/components/BoardCard/BoardCard.jsx
@@ -1,15 +1,16 @@
 // Jada
 //Ayo edited this so that the view button actually takes you to see the cards
 import "./BoardCard.css";
-import KudosPage from "../KudosPage/KudosPage";
-import { Link } from "react-router-dom";
+
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { MdOutlineDelete } from "react-icons/md";
 
 function BoardCard({ board, onDelete }) {
   const navigate = useNavigate();
 
-  const handleDelete = async () => {
+  const handleDelete = async (e) => {
+    e.stopPropagation(); // Prevent card click when delete button is clicked
     try {
       await axios.delete(`http://localhost:3000/boards/${board.id}`);
       if (onDelete) onDelete();
@@ -17,9 +18,17 @@ function BoardCard({ board, onDelete }) {
       alert("Failed to delete card.", err);
     }
   };
+
+  const handleCardClick = () => {
+    navigate(`/boards/${board.id}/cards`);
+  };
+
   return (
     <>
-      <div className="board">
+      <div className="board" onClick={handleCardClick}>
+        <span className="delete-btn" onClick={handleDelete}>
+            <MdOutlineDelete />
+        </span>
         <div className="img-container">
           <img src={board.image_url} alt="board image"></img>
         </div>
@@ -27,17 +36,6 @@ function BoardCard({ board, onDelete }) {
           <h3>{board.title}</h3>
           <p>{board.description}</p>
           <span className="category">{board.category}</span>
-        </div>
-        <div className="buttons">
-          <button
-            className="option"
-            onClick={() => navigate(`/boards/${board.id}/cards`)}
-          >
-            view
-          </button>
-          <button className="option" onClick={handleDelete}>
-        delete
-          </button>
         </div>
       </div>
     </>
